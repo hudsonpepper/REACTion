d17a96bb99c6a30a54b1dbb6b8df199e24513fb7
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2024-02-12 14:37:11 -0500

    fixed targets jumping each second

--- client/src/pages/GamePage.jsx
+++ client/src/pages/GamePage.jsx
@@ -1,66 +1,16 @@
 import Target from '../components/Target'
-import { useState } from 'react';
-import { ReactDOM } from 'react';
-import { createPortal } from 'react-dom';
+import GameUI from '../components/GameUI'
+import { useTheme } from '../utils/GameContext';
 
-const GamePage = () => {
-    const [gameState, setGameState] = useState(0);
-    const [targetCounter, setTargetCounter] = useState(0);
-    const [countdownClock, setCountdownClock]= useState(20);
-    let intervalId;
+const GamePage = () => {    
 
-    const readyHandler = (e) => {
-        e.preventDefault();
-        e.target.setAttribute("disabled", "");
-        e.target.setAttribute("hidden", "");
-
-        let clockActual = countdownClock;
-        setTargetCounter(0);
-        setGameState(1);
-        // call render target
-        renderTarget(e);
-        // start timer for game
-        intervalId = setInterval(() => {
-            clockActual = clockActual - 1;
-            setCountdownClock(clockActual);
-            if (clockActual <= 0) {
-                setGameState(0);
-                e.target.removeAttribute("disabled", "");
-                e.target.removeAttribute("hidden", "");
-                clearInterval(intervalId)
-                setCountdownClock(20);
-            }
-        }, 1000);
-    }
-
-    const renderTarget = (e) => {
-        if (gameState != 0) {
-            setGameState(gameState * -1);
-            setTargetCounter(targetCounter + 1);
-        }
-        if (targetCounter >= 9) {
-            setGameState(0);
-            e.target.parentElement.children[0].removeAttribute("disabled", "");
-            e.target.parentElement.children[0].removeAttribute("hidden", "");
-            clearInterval(intervalId);
-            // console.log(targetCounter, countdownClock);
-        }
-    }
+    const {gameState, renderTarget} = useTheme();
 
     return (
         <div className='overarching-container'>
-            <div className='game-header-bar'>
-                <h1>REACTion</h1>
-                <ul>
-                    <li>Profile / Sign-Up</li>
-                    <li>Leaderboard</li>
-                </ul>
-                {gameState == 0 ? null : <h2>TIME REMAINING: {countdownClock}</h2>}
-                {gameState == 0 ? null : <h2>TARGETS HIT: {targetCounter}</h2>}
-            </div>
+            <GameUI />
 
             <div className='game-viewport'>
-                <button onClick={readyHandler}>Ready?</button>
                 {gameState == 0 ? null : <Target onClickHandler={renderTarget}/>}
             </div>
         </div>
