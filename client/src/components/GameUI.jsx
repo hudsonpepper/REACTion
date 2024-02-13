b89f2906dd37f03e576aaf98cacb5c7454fcfefe
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2024-02-13 14:18:07 -0500

    added style, small tweaks

--- client/src/components/GameUI.jsx
+++ client/src/components/GameUI.jsx
@@ -6,7 +6,8 @@ const GameUI = () => {
     
     return (
         <div className='game-header-bar'>
-            {gameState == 0 ? <button onClick={readyHandler}>Ready?</button> : null }
+            {gameState == 0 ? <h2>Hit all the targets as fast as you can!</h2> : null }
+            {gameState == 0 ? <button className="ready-btn" onClick={readyHandler}>Ready?</button> : null }
             {gameState == 0 ? null : <h2>TIME REMAINING: {countdownClock}</h2>}
             {gameState == 0 ? null : <h2>TARGETS HIT: {targetCounter}</h2>}
         </div>
