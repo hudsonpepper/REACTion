7840d0074e7abf924c6c01ec5c086d8fd5b24463
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2024-02-15 12:25:22 -0500

    added alt text to footer photos

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -14,8 +14,8 @@ const Footer = () => {
             &larr; Go Back
           </button>
         )} */}
-        <a href="https://github.com/hudsonpepper/REACTion/graphs/contributors"><img className="h-10 px-2" src={Logo} /></a>
-        <a href="https://github.com/hudsonpepper/REACTion"><img className="h-10 px-2"src={GithubLogo} /></a>
+        <a href="https://github.com/hudsonpepper/REACTion/graphs/contributors"><img className="h-10 px-2" src={Logo} alt="Logo and Link to Contributers Page"/></a>
+        <a href="https://github.com/hudsonpepper/REACTion"><img className="h-10 px-2"src={GithubLogo} alt="Github Logo and Link to Repository"/></a>
         {/* <img className="sm: size-4 md:size-10 justify-center" src={Logo} /> */}
       </div>
     </footer>
